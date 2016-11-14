dc69adcd934ebf3a3271cfb0372dc4ac74946f8c
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-11-14 10:02:02 -0700

    Polish docstrings

--- code/gradientdescent.py
+++ code/gradientdescent.py
@@ -14,13 +14,13 @@ class GradientDescent:
     """
     def __init__(self, loss='log', penalty='none', n_iter=5, eta0=0.0, alpha=0.0001):
         """
-        The GradientDescent constructor
+        The GradientDescent constructor.
 
         Arguments:
             loss -- A string representing the type of loss function to use. Defaults to 'hinge'. 
                     Options include 'log', 'hinge', and 'perceptron'.
                     See http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDClassifier.html#sklearn.linear_model.SGDClassifier
-                    for details
+                    for details.
             penalty -- A string representing the type of regularizer to use. Defaults to 'none'.
                        Options include 'l2', 'l1', 'elasticnet', or 'none'.
             n_iter -- An integer representing the number of times to iterate over the data while
@@ -40,7 +40,7 @@ class GradientDescent:
             data -- A numpy array representing the training data.
             labels -- A numpy array representing the training labels.
                       Must be N rows x 1 column, where N is the number of
-                      training data examples
+                      training data examples.
         """
         self.classifier.fit(data, labels)
 
@@ -52,7 +52,7 @@ class GradientDescent:
             data -- A numpy array representing the testing data.
             labels -- A numpy array representing the testing labels.
                       Must be N rows x 1 column, where N is the number of
-                      testing data examples
+                      testing data examples.
         """
         predictions = self.classifier.predict(data)
         error = mean(predictions != labels)
